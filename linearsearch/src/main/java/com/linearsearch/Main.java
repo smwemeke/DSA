c81868ea8f86cbfc3d523db7ee49acf01677afe7
smwemeke <sue.mwemeke@gmail.com>
2024-02-10 21:41:29 -0600

    add changes

--- linearsearch/src/main/java/com/linearsearch/Main.java
+++ linearsearch/src/main/java/com/linearsearch/Main.java
@@ -2,6 +2,24 @@ package com.linearsearch;
 
 public class Main {
     public static void main(String[] args) {
+        int [] arr = {3,16,30,65,12, 40,50,29,25};
+        int target = 40;
+        int ans = linearSearch(arr, target);
+        System.out.println("Index is: " + ans);
 
+    }
+    // search the in the array: return the index if item found
+    // otherwise return -1 if item not found
+    static int linearSearch (int arr [], int target){
+        // check for element at every index if it is = target
+        if (arr.length == 0){
+            return -1;
+        }
+        for (int index = 0; index < arr.length; index++){
+            if (arr[index] == target){
+                return index;
+            }
+        }
+        return -1;
     }
 }
