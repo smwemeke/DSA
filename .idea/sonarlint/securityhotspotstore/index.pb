d29f5d34f7b895f5f158d3e243ddd7c6f8b5629b
smwemeke <sue.mwemeke@gmail.com>
2024-02-10 22:44:59 -0600

    added changes

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,7 @@
+
+D
+linearsearch/pom.xml,d\2\d2d86104f7b709191103ef621aafa192fd414863
+e
+5linearsearch/src/main/java/com/linearsearch/Main.java,8\7\87fc86aabd2b651903ad9183113f9bcc9b8f4659
+p
+@linearsearch/src/main/java/com/linearsearch/SearchInStrings.java,8\2\8214b1ec7b4e22cc9a03f83c2841fbfaea0574a0
--- linearsearch/src/main/java/com/linearsearch/Main.java
+++ linearsearch/src/main/java/com/linearsearch/Main.java
@@ -3,23 +3,55 @@ package com.linearsearch;
 public class Main {
     public static void main(String[] args) {
         int [] arr = {3,16,30,65,12, 40,50,29,25};
-        int target = 40;
-        int ans = linearSearch(arr, target);
+        int element = 16;
+        int ans = linearSearch(arr, element);
         System.out.println("Index is: " + ans);
 
+        System.out.println(" " );
+
+        int ans2 = linearSearch2(arr, element);
+        System.out.println("Element is: " + ans2);
     }
     // search the in the array: return the index if item found
     // otherwise return -1 if item not found
     static int linearSearch (int arr [], int target){
-        // check for element at every index if it is = target
         if (arr.length == 0){
             return -1;
         }
+//        // check for element at every index if it is = target
         for (int index = 0; index < arr.length; index++){
             if (arr[index] == target){
                 return index;
             }
         }
+        // if none of the return statements above have executed and target value has not been found
         return -1;
+    }
+    // return element itself instead of index
+    static  int linearSearch2(int [] arr, int target){
+        if (arr.length == 0){
+            return -1;
+        }
+        // using enhanced for loop
+        for (int element : arr){
+            if (element == target){
+                return element;
+            }
+        }
+        return -1;
+    }
+
+    // return true or false itself instead of index
+    static  boolean linearSearch3(int [] arr, int target){
+        if (arr.length == 0){
+            return false;
+        }
+        // using enhanced for loop
+        for (int element : arr){
+            if (element == target){
+                return true;
+            }
+        }
+        return false;
     }
 }
